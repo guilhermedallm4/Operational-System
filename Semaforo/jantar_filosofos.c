a5b3c18f6323f31c011f006b0cfd265f79f02f87
Guilherme D. Lima <54902373+guilhermedallm4@users.noreply.github.com>
2022-09-13 20:49:06 -0300

    implemented Semaphore for exercice

--- Semaforo/jantar_filosofos.c
+++ Semaforo/jantar_filosofos.c
@@ -11,10 +11,10 @@ int hashisInUse = 0;
 
 void *utilizandoHashi(int filosofo){
 
-        if(hashisInUse < 4){
+        if(hashisDisponible > 1 && (hashisInUse + 2) <= 4){
             sem_wait(&mutex);
-            hashisInUse += 2;
             hashisDisponible -= 2;
+            hashisInUse += 2;
             while(hashisDisponible <= 1 && hashisInUse >= 4){
                 printf("Filosofo %d, Wating of Hashi is disponible\n", filosofo);
                 sleep(4);
@@ -25,6 +25,7 @@ void *utilizandoHashi(int filosofo){
             printf("Filosofo: %d\nHashisDisponible: %d\nHashisUsed: %d\n", filosofo, hashisDisponible, hashisInUse);
             hashisDisponible+=2;
             hashisInUse -= 2;
+            printf("Filosofo: %d\nHashisDisponible: %d\nHashisUsed: %d\n", filosofo, hashisDisponible, hashisInUse);
             sleep(4);
             sem_post(&mutex);
         }
@@ -42,6 +43,7 @@ void *utilizandoHashi(int filosofo){
             printf("Filosofo: %d\nHashisDisponible: %d\nHashisUsed: %d\n", filosofo, hashisDisponible, hashisInUse);
             hashisDisponible+=2;
             hashisInUse -= 2;
+            printf("Filosofo: %d\nHashisDisponible: %d\nHashisUsed: %d\n", filosofo, hashisDisponible, hashisInUse);
             sleep(4);
             sem_post(&mutex);
         }
