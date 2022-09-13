d0613f152e1b6fec8b24be6de6857fe2c476a64a
Guilherme D. Lima <54902373+guilhermedallm4@users.noreply.github.com>
2022-09-13 20:14:38 -0300

    Semaphore implemented

--- Semaforo/jantar_filosofos.c
+++ Semaforo/jantar_filosofos.c
@@ -10,49 +10,56 @@ int hashisDisponible = 5;
 int hashisInUse = 0;
 
 void *utilizandoHashi(int filosofo){
-    if(hashisInUse < hashi && hashisDisponible != 4){
-        sem_wait(&mutex);
-        hashisInUse += 2;
-        hashisDisponible -= 2;
-        printf("I filoso %d rungry\n", filosofo);
-        //sleep(4);
-        printf("Filoso %d in pensament\n", filosofo);
-        //sleep(4);
-        printf("Finalized");
-        hashisDisponible+=2;
-        hashisInUse -= 2;
-        sem_post(&mutex);
-    } 
-    else{
-        printf("%d", hashisDisponible);
-        while(hashisDisponible == 1){
-            printf("Filoso %d Aguardando hashi", filosofo);
+
+        if(hashisInUse < 4){
+            sem_wait(&mutex);
+            hashisInUse += 2;
+            hashisDisponible -= 2;
+            while(hashisDisponible <= 1 && hashisInUse >= 4){
+                printf("Filosofo %d, Wating of Hashi is disponible\n", filosofo);
+                sleep(4);
+            }
+            printf("I filoso %d rungry\n", filosofo);
+            printf("Filoso %d in pensament\n", filosofo);
+            printf("Finalized\n");
+            printf("Filosofo: %d\nHashisDisponible: %d\nHashisUsed: %d\n", filosofo, hashisDisponible, hashisInUse);
+            hashisDisponible+=2;
+            hashisInUse -= 2;
+            sleep(4);
+            sem_post(&mutex);
         }
-        sem_wait(&mutex);
-        hashisInUse += 2;
-        hashisDisponible -= 2;
-        printf("I filoso %d rungry\n", filosofo);
-        //sleep(4);
-        printf("Filoso %d in pensament\n", filosofo);
-        //sleep(4);
-        printf("Finalized");
-        hashisDisponible+=2;
-        hashisInUse -= 2;
-        sem_post(&mutex);
+        else{
+            while(hashisDisponible <= 1 && hashisInUse >= 4){
+                printf("Filosofo %d, Wating of Hashi is disponible\n", filosofo);
+                sleep(4);
+            }
+            sem_wait(&mutex);
+            hashisInUse += 2;
+            hashisDisponible -= 2;
+            printf("I filoso %d rungry\n", filosofo);
+            printf("Filoso %d in pensament\n", filosofo);
+            printf("Finalized\n");
+            printf("Filosofo: %d\nHashisDisponible: %d\nHashisUsed: %d\n", filosofo, hashisDisponible, hashisInUse);
+            hashisDisponible+=2;
+            hashisInUse -= 2;
+            sleep(4);
+            sem_post(&mutex);
+        }
 
-    }
 
+    
 }
 
 int main (){
 
     int i;
     pthread_t tid[hashi];
+    sem_init(&mutex, 0, 5);
     for(i = 0; i<5;i++){
         pthread_create(&tid[i], NULL, utilizandoHashi, i);
     }
     for(i = 0;i<5;i++){
-        pthread_join(&tid[i], NULL);
+        pthread_join(tid[i], NULL);
     }
+    sem_destroy(&mutex);
 }
-
